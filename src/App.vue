25fafdcb07399c121129b670f1b4514e41fc9ea9
Ustych Maksym <ustichmaxim@gmail.com>
2020-09-05 23:33:46 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="w-full max-w-screen-xl mx-auto">
+    <div class="flex"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+};
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>index page</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
